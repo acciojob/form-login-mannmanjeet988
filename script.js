152e68e47ed3fcd40b23f10177e851f856dab726
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-20 13:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+const inputElements= document.getElementsByTagName("input");
+	
+		const  firstname= inputElements[0].value;
+	const lastname= inputElements[1].value;
 
+	alert(firstname + " " + lastname);
+	
 }
